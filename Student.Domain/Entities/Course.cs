e8f08e869fd0ade85e75eb2f32aab44a8535de6e
Alberto Santamaria <alberto.santamaria@animaeducacao.com.br>
2023-07-19 11:14:15 -0300

    Modificação do DataAnnotations

--- Student.Domain/Entities/Course.cs
+++ Student.Domain/Entities/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace Student.Domain.Entities
 {
     public class Course
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100,ErrorMessage = "O Curso não pode ter mais do que cem caracteres")]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
 
     }
--- Student.Domain/Entities/Studenten.cs
+++ Student.Domain/Entities/Studenten.cs
@@ -1,13 +1,26 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Student.Domain.Entities
 {
     public class Studenten
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+        [Required(ErrorMessage = "O nome não pode ser vazio")]
+        [MaxLength(100)]
+        [MinLength(2,ErrorMessage = "O nome não pode ter menos do que dois caracteres")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "O endereço não pode ser vazio")]
+        [MaxLength(300)]
+        [MinLength(20, ErrorMessage = "O endereço não pode ter menos de vintes caracteres")]
+        public string? Address { get; set; }
+
         [JsonIgnore]
         public Course Course { get; set; }
     }
 }
+
+

da27d34f87aab1d7d307798134f7afcc5ddffdde
Raissa Arruda <raissa.arruda@animaeducacao.com.br>
2023-07-28 00:08:30 -0300

    json ignore

--- Student.Application/Dtos/StudentCourseLessonDto.cs
+++ Student.Application/Dtos/StudentCourseLessonDto.cs
@@ -1,14 +1,22 @@
-﻿namespace Student.Application.Dtos
+﻿using System.Text.Json.Serialization;
+
+namespace Student.Application.Dtos
 {
     public class StudentCourseLessonDto
     {
+        [JsonIgnore]
         public int StudentCourseLessonId { get; set; }
         public int LessonId { get; set; }
         public int CourseId { get; set; }
+   
         public int StudentenId { get; set; }
+        [JsonIgnore]
         public LessonDto LessonDto { get; set; }
+        [JsonIgnore]
         public CourseDto CourseDto { get; set; }
+        [JsonIgnore]
         public StudentDto StudentDto { get; set; }
+        [JsonIgnore]
         public bool IsCompleted { get; set; }
     }
 }
